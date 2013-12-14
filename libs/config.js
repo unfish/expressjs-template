db53376a3f9bf41ae81b113ba6908941236772ba
Jason.Cui <jason.cui@126.com>
2013-12-15 00:10:59 +0800

    add global config file

---
+++ libs/config.js
@@ -0,0 +1,6 @@
+module.exports = function() {
+    return {
+        Site:{Title:"Unfish's CRM",Keywords:"CRM,模板,Node.js,Express,Jade,Mongoose",Description:"这是一个简单的CRM系统模板",Author:"Unfish"}
+        ,DB:{MongoDB:'mongodb://localhost/mydb',FileDB:'mongodb://localhost/Files'}
+    };
+}();
