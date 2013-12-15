efabe9ac1853b8edfe0b12294aa6f7cfc36ce508
Jason.Cui <jason.cui@126.com>
2013-12-15 22:32:45 +0800

    fixed

--- controllers/admin/index.js
+++ controllers/admin/index.js
@@ -7,7 +7,7 @@ var t = require('../../models/topic'),
 module.exports.controller = function(app) {
     app.get('/admin', User.NeedAdminGET, function(req, res) {
         var ep = EventProxy.create("topicCount", "userCount", function (topicCount, userCount) {
-            res.render('admin/index',{ pageTitle: '管理中心', pageTips: '欢迎登录'+app.locals.Site.Title+'管理中心，上次登录时间：<span class="timeSpan">'+req.user.lastlogin_at+'</span>，上次登录IP：'+req.user.lastlogin_ip, userCount: userCount, topicCount:topicCount });
+            res.render('admin/index',{ pageTitle: '管理中心', pageTips: '欢迎登录'+app.locals.Site.Title+'管理中心', userCount: userCount, topicCount:topicCount });
         });
         User.count({},function (err, count) {
             ep.emit('userCount', count);
