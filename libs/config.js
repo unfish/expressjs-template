10c852819b8843c27914d7d04f9485fc442bc374
Jason.Cui <jason.cui@126.com>
2013-12-15 21:58:22 +0800

    change footer content

--- libs/config.js
+++ libs/config.js
@@ -1,8 +1,8 @@
 module.exports = function() {
     return {
-        Site:{Title:"Unfish's CRM"
-            ,Keywords:"CRM,模板,Node.js,Express,Jade,Mongoose"
-            ,Description:"这是一个简单的CRM系统模板"
+        Site:{Title:"Unfish's Demo Site"
+            ,Keywords:"CRM,模板,Template,Node.js,Express,Jade,Mongoose"
+            ,Description:"这是一个简单的CRM系统模板，基于Nodejs+Express+Mongoose，你可以在此基础上方便的开发出自己的网站系统"
             ,Author:"Unfish"
         }
         ,DB:{DataDB:'mongodb://localhost/DataDB'
