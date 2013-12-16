b6150a501b1400ab34c7b638cd424ae38f8e9b71
Jason.Cui <jason.cui@126.com>
2013-12-16 16:18:08 +0800

    fixed

--- libs/config.js
+++ libs/config.js
@@ -1,5 +1,4 @@
-module.exports = function() {
-    return {
+module.exports =  {
         Site:{Title:"Unfish's Demo Site"
             ,Keywords:"CRM,模板,Template,Node.js,Express,Jade,Mongoose"
             ,Description:"这是一个简单的CRM系统模板，基于Nodejs+Express+Mongoose，你可以在此基础上方便的开发出自己的网站系统"
@@ -10,4 +9,3 @@ module.exports = function() {
             ,LogDB:'mongodb://localhost/LogDB'
         }
     };
-}();
